6bf7a24363f0449d642f20599cd21e75c08e032a
Marcelo Severo <git@github.com:marceloses/APICatalogo.git>
2025-01-07 07:53:56 -0300

    16. Criando o Modelo de dominio

---
+++ LanchesMac/Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿namespace LanchesMac.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string CategoriaNome { get;}
+        public string Descricao { get;}
+
+        // Lista Lanches
+        public List<Lanche> Lanches { get;}
+    }
+}
